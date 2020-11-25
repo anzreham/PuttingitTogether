35aa9347c4f7f94d370f70d7cdb4bd43a6c73ed7
reham <rehamal943@gmail.com>
2020-11-25 16:23:59 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import {SomeClassComponent, MyNewComponent} from './components/MyNewComponent.js';
+
+import PersonCard from './components/MyNewComponent.js';
+// import {SomeClassComponent, MyNewComponent} from './components/MyNewComponent.js';
 
 function App() {
  
   return (
     <div className="App">
-      <SomeClassComponent firstname = {"Doe "}  lastname = {"Jane"} age = {45} hairColor = {"Black"}/>
+   <PersonCard firstname = {"Doe "}  lastname = {"Jane"} age = {45} hairColor = {"Black"}/>
+
+   <PersonCard firstname = {"Smith "}  lastname = {"John"} age = {88} hairColor = {"Brown"} /> 
+
+    <PersonCard firstname = {"Fillmore"} lastname = {"Millard"} age = {"50"}  hairColor = {"Brown"}/>
+
+   <PersonCard firstname = {"Smith "} lastname = {"Maria"} age = {"62"}  hairColor = {"Brown"} /> 
+
+
+</div>
+    
+    );
+  }
+      {/* <SomeClassComponent firstname = {"Doe "}  lastname = {"Jane"} age = {45} hairColor = {"Black"}/>
 
     
  
       
 
-       <SomeClassComponent firstname = {"Smith "}  lastname = {"John"} age = {88} hairColor = {"Brown"} />
+       <SomeClassComponent firstname = {"Smith "}  lastname = {"John"} age = {88} hairColor = {"Brown"} /> */}
     
 
 
@@ -30,9 +45,6 @@ function App() {
 
 
        
-    </div>
-    
-  );
-}
+
 
 export default App;
--- src/components/MyNewComponent.js
+++ src/components/MyNewComponent.js
@@ -1,46 +1,55 @@
 import React, { Component } from 'react';
+const PersonCard = props => {
+    return(
+        <div>
+            <h1>{ props.firstname }, { props.lastname }</h1>
+            <p>Age: { props.age }</p>
+            <p>Hair Color: { props.hairColor }</p>
+        </div>
+    );
+}
+export default PersonCard;
     
-    
-class SomeClassComponent extends Component {
-    state = {
-        count: this.props.age 
-      };
-    handleClick = () => {
-        this.setState((prevState, { count }) => ({
-          count: prevState.count + 1
-        }));
-      };
-    render() {
-    const { firstname, lastname, age, hairColor } = this.props;
+// class SomeClassComponent extends Component {
+//     state = {
+//         count: this.props.age 
+//       };
+//     handleClick = () => {
+//         this.setState((prevState, { count }) => ({
+//           count: prevState.count + 1
+//         }));
+//       };
+//     render() {
+//     const { firstname, lastname, age, hairColor } = this.props;
  
-        return <div>
+//         return <div>
 
- <h1> { firstname}  { lastname }</h1>
-<p> Age: {this.state.count} </p>
-<p> Hair color {hairColor} </p>
+//  <h1> { firstname}  { lastname }</h1>
+// <p> Age: {this.state.count} </p>
+// <p> Hair color {hairColor} </p>
 
-<button onClick={this.handleClick}> click me</button>
+// <button onClick={this.handleClick}> click me</button>
   
 
          
-</div>;
-    }
-}
+// </div>;
+//     }
+// }
 
 
 
 
-class MyNewComponent extends Component{
-    render(){
-        return(
-            <div>
-                <h1>
-                    { this.props.header }
-                </h1>
-                { this.props.children }
-            </div>
-        );
-    }
-}
+// class MyNewComponent extends Component{
+//     render(){
+//         return(
+//             <div>
+//                 <h1>
+//                     { this.props.header }
+//                 </h1>
+//                 { this.props.children }
+//             </div>
+//         );
+//     }
+// }
 
-export  { MyNewComponent, SomeClassComponent };
+// export  { MyNewComponent, SomeClassComponent };
